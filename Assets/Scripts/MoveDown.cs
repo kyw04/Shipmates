c3149f296188e190b8d8c22d43d9cb74cbb4d816
kyw04 <dddd93768@gmail.com>
2023-02-11 17:29:18 +0900

    InGame

---
+++ Assets/Scripts/MoveDown.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+    public float speed;
+    public float scale = 0.25f;
+    public Vector3 direction;
+
+    private Vector3 startScale;
+    
+    private void Start()
+    {
+        Destroy(gameObject, 5f);
+        startScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        speed += Time.deltaTime;
+
+        transform.position += direction * speed * Time.deltaTime;
+        transform.localScale += startScale * 2 * scale * Time.deltaTime;
+    }
+}
