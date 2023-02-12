e67d271d1906a503596a57c2c18e35b561d69609
TypiDoyun <121522665+TypiDoyun@users.noreply.github.com>
2023-02-12 15:40:47 +0900

    Update GameOverScript.cs

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Collections;
 
 public class GameOverScript : MonoBehaviour, IPointerDownHandler
 {
@@ -24,20 +25,9 @@ public class GameOverScript : MonoBehaviour, IPointerDownHandler
         people = PlayerPrefs.GetInt("People");
         treasure = PlayerPrefs.GetInt("Treasure");
         bestScore = PlayerPrefs.GetInt("BestScore");
-        currentScore = score + (people * 500) + (treasure * 300); 
+        currentScore = score + (people * 500) + (treasure * 300);
         step = (int) Mathf.Max(Mathf.Pow(10, Mathf.FloorToInt(Mathf.Log10(currentScore))) / 1000 + Random.Range(-100, 100), 1);
-    }
-
-    private void Update()
-    {
-
-        progressScore = Mathf.Min(currentScore, progressScore + step);
-        currentScoreGUI.text = progressScore.ToString();
-
-        if (progressScore != currentScore) return;
-        if (bestScore < currentScore) bestScore = currentScore;
-
-        bestScoreGUI.text = bestScore.ToString();
+        StartCoroutine(Score());
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -47,7 +37,25 @@ public class GameOverScript : MonoBehaviour, IPointerDownHandler
 
     public void changeScene()
     {
-        PlayerPrefs.SetInt("BestScore", bestScore);
+        progressScore = 0;
         SceneManager.LoadScene("Lobby");
     }
+
+    IEnumerator Score()
+    {
+        while (true)
+        {
+            progressScore = Mathf.Min(currentScore, progressScore + step);
+            currentScoreGUI.text = progressScore.ToString();
+
+            yield return new WaitForSeconds(0.05f);
+
+            if (currentScore == progressScore) break;
+        }
+
+        if (bestScore < currentScore) bestScore = currentScore;
+
+        bestScoreGUI.text = bestScore.ToString();
+    }
+
 }
